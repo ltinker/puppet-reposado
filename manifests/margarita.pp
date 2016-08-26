6b19d10cc05edfac32d68e4a0858d5d96a6ca701
Luke Tinker <luke.tinker@anu.edu.au>
2016-08-26 10:12:24 +1000

    Added class variable for $manage_margarita_service default as true, also reformatted other class variables to be inline.

--- manifests/margarita.pp
+++ manifests/margarita.pp
@@ -1,23 +1,21 @@
 class reposado::margarita (
-  $user                    = $::reposado::params::user,
-  $group                   = $::reposado::params::group,
-  $server_name             = $::reposado::params::server_name,
-  $git_ensure              = $::reposado::params::git_ensure,
-  $git_revision            = $::reposado::params::service_dir,
-  $margarita_root          = "${::reposado::params::base_dir}/margarita",
-  $margarita_git_source    = $::reposado::params::margarita_git_source,
-  $margarita_git_revision  = $::reposado::params::margarita_git_revision,
-  $manage_flask            = true,
-  $manage_margarita        = true,
-  $manage_simplejson       = true,
-  $preferences_link        = "${margarita_root}/preferences.plist",
-  $reposadolib_link        = "${margarita_root}/reposadolib",
-  $reposado_root           = $::reposado::params::reposado_root,
-  $system_services         = $::reposado::params::system_services,)
+  $user                     = $::reposado::params::user,
+  $group                    = $::reposado::params::group,
+  $server_name              = $::reposado::params::server_name,
+  $git_ensure               = $::reposado::params::git_ensure,
+  $margarita_git_revision   = $::reposado::params::margarita_git_revision,
+  $margarita_root           = "${::reposado::params::base_dir}/margarita",
+  $manage_margarita_service = true,
+  $margarita_git_source     = $::reposado::params::margarita_git_source,
+  $margarita_git_revision   = $::reposado::params::margarita_git_revision,
+  $manage_flask             = true,
+  $manage_simplejson        = true,
+  $preferences_link         = "${margarita_root}/preferences.plist",
+  $reposadolib_link         = "${margarita_root}/reposadolib",
+  $reposado_root            = $::reposado::params::reposado_root,
 
 
   {
-    if $manage_margarita {
 
   vcsrepo { $margarita_root:
     ensure   => $git_ensure,
@@ -28,7 +26,7 @@ class reposado::margarita (
     source   => $margarita_git_source,
     revision => $git_revision;
 
-    }
+    }->
     file {
       [$margarita_root]:
         ensure => 'directory',
@@ -36,24 +34,47 @@ class reposado::margarita (
         group  => $group,
         mode   => '0755';
 
-      "${system_services}/margarita":
-        ensure  => 'present',
-        owner   => $user,
-        group   => $group,
-        mode    => '0755',
-        content => template('reposado/margarita.initd.erb'),
-        require => Vcsrepo[$margarita_root];
-
       "$reposadolib_link":
         ensure  => 'link',
+        owner   => $user,
+        group   => $group,
         target  => "${reposado_root}/code/reposadolib",
         require => Vcsrepo[$reposado_root];
       "$preferences_link":
         ensure  => 'link',
+        owner   => $user,
+        group   => $group,
         target  => "${reposado_root}/code/preferences.plist",
         require => File["${reposado_root}/code/preferences.plist"];
     }
+  if $manage_margarita_service{
+    if $facts['initservice'] == 'initd'{
+        $system_services = '/etc/init.d'
+        file{"${system_services}/margarita":
+          ensure  => 'present',
+          owner   => $user,
+          group   => $group,
+          mode    => '0755',
+          content => template('reposado/margarita.initd.erb'),
+          require => Vcsrepo[$margarita_root];
+        }
+    }
   }
+    elsif $facts['initservice'] == 'systemd'{
+        $system_services = '/etc/systemd/system'
+        file{"${system_services}/margarita.service":
+          ensure  => 'present',
+          owner   => $user,
+          group   => $group,
+          mode    => '0755',
+          content => template('reposado/margarita.systemd.erb'),
+          require => Vcsrepo[$margarita_root];
+          }
+    }
+    else {
+      warning("The ${module_name} Module does not support management of margarita as a service on ${::osfamily}")
+    }
+
 
 }
 
